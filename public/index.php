252957f95fe2492c958198f43b940c90f8ea8026
Your Name <you@example.com>
2026-06-04 18:28:30 +0000

    from prod

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
+    if ($_ENV['FORCE_HTTPS'] ?? false) {
+        $_SERVER['REQUEST_SCHEME'] = 'https';
+        $_SERVER['HTTPS'] = 'on';
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
