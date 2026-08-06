9076f6f347d16b7a627be8ac659210533aa41d61
Maxim Gabidullin <after@ya.ru>
2026-08-06 18:16:05 +0300

    status-time-in-atom

--- src/Event/Handler/UserStatusUpdatedEventHandler.php
+++ src/Event/Handler/UserStatusUpdatedEventHandler.php
@@ -28,7 +28,7 @@ final readonly class UserStatusUpdatedEventHandler
                     'bgColor' => $event->status->bgColor,
                     'iconUrl' => $event->status->iconUrl,
                 ],
-                'statusTime' => $event->status->statusTime,
+                'statusTime' => $event->status->statusTime?->format(\DateTimeInterface::ATOM),
             ])
         );
 
